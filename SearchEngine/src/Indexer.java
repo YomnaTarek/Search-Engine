8e2a056674aaef25d48b9612c05429ceab0e3318
iamshaunjp <mariam.ali00@eng-st.cu.edu.eg>
2022-05-06 21:03:28 +0200

    query functions for stemmer.

--- SearchEngine/src/Indexer.java
+++ SearchEngine/src/Indexer.java
@@ -7,7 +7,6 @@ public class Indexer {
 	}
 	
 	 private static class Index extends Thread{
-		 
-	 }
+
 	
-}
+     }
--- SearchEngine/src/IndexerDbConnection.java
+++ SearchEngine/src/IndexerDbConnection.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
@@ -32,10 +33,72 @@ public class IndexerDbConnection {
 		         System.out.print("Database Not Connected"+e);
 		     }
 		  }
-		
-		public static void main(String args[]) throws Exception {
+
+	    //Reset beginIndexing to 0 for all the words that their urls have started indexing but are not finished yet(endIndexing=0). 
+	    static public void ResetAlreadyStarted(Integer numThreads) throws SQLException {  //nondone
+			String foundLinks="";
+			PreparedStatement prepStatement= conn.prepareStatement("SELECT url FROM Link WHERE beginIndexing =1 and endIndexing=0 limit "+numThreads.toString(), Statement.RETURN_GENERATED_KEYS);
+			ResultSet result=prepStatement.executeQuery();
+			if(result.next())
+			{
+				foundLinks=result.getString(1);
+				if(foundLinks==null)
+				    foundLinks="";
+			}
+			if(foundLinks.length()!=0)
+			{
+			    prepStatement= conn.prepareStatement("UPDATE Link set beginIndexing=0 WHERE beginIndexing=1 and endIndexing=0", Statement.RETURN_GENERATED_KEYS);
+                prepStatement.executeUpdate();
+			}
+		}
+
+		//Finding an unindexed url and returning it.
+		  static public String getUnIndexedUrl() throws SQLException {
+			String  unindexed=null;
+			PreparedStatement prepStatement= conn.prepareStatement("SELECT url FROM Link WHERE endIndexing =0 and beginIndexing=0 limit 1", Statement.RETURN_GENERATED_KEYS);
+			ResultSet result = prepStatement.executeQuery();
+			if (result.next()) 
+			{
+				unindexed= result.getString(1);
+		    }
+			return unindexed;
+		}
+        //Start indexing a url by setting beginIndexing.
+		static public void BeginIndexing(String urlStarted)  throws SQLException {
+			PreparedStatement prepStatement = conn.prepareStatement("UPDATE Link set beginIndexing=1 WHERE url=?", Statement.RETURN_GENERATED_KEYS );
+			prepStatement.setString(1, urlStarted);
+			prepStatement.executeUpdate();
+		}
+        //Start indexing a url by setting endIndexing.
+		static public void EndIndexing(String urlDone) throws SQLException {
+			PreparedStatement prepStatement = conn.prepareStatement("UPDATE Link set endIndexing=1 WHERE url=?", Statement.RETURN_GENERATED_KEYS );
+			prepStatement.setString(1, urlDone);
+			prepStatement.executeUpdate();
+		}
+
+		//Reseting the values of the attributes endIndexing and beginIndexing in the Link table for all the urls.
+		static public void zeroingEndAndBegin() throws SQLException {
+			PreparedStatement prepStatement= conn.prepareStatement( "UPDATE Link set endIndexing=0,beginIndexing=0", Statement.RETURN_GENERATED_KEYS );
+			prepStatement.executeUpdate();
+		}
+
+		//Dropping the IndexingWords Table.
+		static public void deleteIndexingWordsTable() throws SQLException {
+			PreparedStatement prepStatement= conn.prepareStatement("DELETE FROM IndexingWords", Statement.RETURN_GENERATED_KEYS );
+			prepStatement.executeUpdate();
+			
+		}
+
+	    static public void IncrementID() throws SQLException{
+		 PreparedStatement prepStatement= conn.prepareStatement("SET @a = -1", Statement.RETURN_GENERATED_KEYS );
+		 prepStatement.executeUpdate();
+		 prepStatement= conn.prepareStatement("UPDATE Link SET id = @a:=@a+1", Statement.RETURN_GENERATED_KEYS );
+		 prepStatement.executeUpdate();
+	   }
+
+	    public static void main(String args[]) throws Exception {
 			IndexerDbConnection id = new IndexerDbConnection();
 			id.DatabaseConnect();
 			return;
-		}
+	    }
 }
